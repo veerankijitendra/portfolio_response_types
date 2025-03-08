947346c0a6f787612df9eaa7d4eb6adb20067951
jitendra veeranki <jitendraveeranki@gmail.com>
2025-03-08 11:29:31 +0530

    adding header response

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,18 @@
 type TStatus = "success" | "failure";
 
-export type TSuccessMessage = {
+export type TSuccessMessage<T> = {
   status: TStatus;
+  data: T;
+  error?: string;
 };
+
+interface ISection {
+  title: string;
+  href: string;
+}
+
+interface IHeader {
+  sections: ISection[];
+}
+
+export type THeaderSuccessMessage = TSuccessMessage<IHeader>;
